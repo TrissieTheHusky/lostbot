e4a82edbdf8ffb1045f35327db700f16a5b76d60
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-18 03:07:14 +0100

    remove unneeded Bot.js file

--- src/models/Bot.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const Bot = new Schema({
-	blacklisted: { type: Array, required: false, default: [] },
-	id: String
-	/* Other data */
-});
-
-module.exports = model('Bot', Bot, 'bot');
