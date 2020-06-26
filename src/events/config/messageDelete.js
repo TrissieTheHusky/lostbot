4f43a57364334ce22eca725bdf83959f91537708
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-26 21:11:34 +0100

    log deleted images

--- src/events/config/messageDelete.js
+++ src/events/config/messageDelete.js
@@ -1,35 +1,32 @@
 module.exports = async (client, message) => {
-	let sa = require('../../models/Guild');
-	let guild = await sa.findOne({ id: message.channel.guild.id });
+	const sa = require('../../models/Guild');
+	const guild = await sa.findOne({ id: message.channel.guild.id });
 
-	let d = guild.logs.action.channel;
+	const d = guild.logs.action.channel;
 	if (message.author.bot !== false) return;
 	if (!d || !guild.logs.action.enabled) return;
 	let o = await message.channel.guild.getAuditLogs(1, null, 72).then((e) => e.entries[0]);
 	let us = o.user.id;
-	let yeet = message.content;
-	if (yeet.length > 360) yeet = `${yeet.slice(0, 360)}\n.....`;
-
+	let embed = {
+		title: 'Message Deleted',
+		color: Number(require('../../config').colours.red),
+		author: { name: ` |  ${message.channel.guild.name}`, icon_url: message.author.avatarURL },
+		description: `A message sent in ${message.channel.mention} which was sent by ${message.author
+			.mention} was deleted.`,
+		fields: [],
+		timestamp: new Date()
+	};
+	if (message.content && !message.attachments.length) {
+		embed.fields.push({ name: 'Content', value: message.content.slice(0, 350), inline: true });
+		embed.fields.push({ name: 'Deleted By', value: `<@!${us}>`, inline: true });
+	} else if (message.attachments.length && message.content) {
+		embed.image = { url: message.attachments[0].proxy_url };
+		embed.fields.push({ name: 'Content', value: message.content.slice(0, 350), inline: true });
+		embed.fields.push({ name: 'Deleted By', value: `<@!${us}>`, inline: true });
+	} else if (message.attachments.length && !message.content) {
+		embed.image = { url: message.attachments[0].proxy_url };
+	}
 	client.createMessage(d, {
-		embed: {
-			title: 'Message Deleted',
-			color: 0xdb1a1a,
-			author: { name: ` |  ${message.channel.guild.name}`, icon_url: message.author.avatarURL },
-			description: `A message sent in ${message.channel.mention} which was sent by ${message.author
-				.mention} was deleted.`,
-			fields: [
-				{
-					name: 'Message Content',
-					value: yeet,
-					inline: true
-				},
-				{
-					name: 'Deleted By',
-					value: `<@!${us}>`,
-					inline: true
-				}
-			],
-			timestamp: new Date()
-		}
+		embed
 	});
 };
