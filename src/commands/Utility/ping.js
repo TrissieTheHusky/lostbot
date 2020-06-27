4cb4d1f8caeb39f1ea83bbe28388072b98fab7a8
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-27 20:27:31 +0100

    remove unneeded comments

--- src/commands/Utility/ping.js
+++ src/commands/Utility/ping.js
@@ -6,27 +6,17 @@ module.exports = {
 	cooldown: 2,
 
 	execute: async (message, client, args) => {
-		if (!args.length) {
-			// const msg = await message.channel.createMessage('🏓 Pong!');
-			// msg.edit(`Pong! - \`${msg.createdAt - msg.createdAt}ms\``);
-			const msg = await message.channel.createMessage('🏓 Pong!');
-			msg.edit(`Pong! - \`${Math.abs(message.createdAt - msg.createdAt)}ms\``);
-		} else if (args[0].toLowerCase() === '0') {
-			message.channel.createMessage(`**Pong!** Shard 0 - \`${client.shards.get(0).latency}ms\``);
-		} else if (args[0].toLowerCase() === '1') {
-			message.channel.createMessage(`**Pong!** Shard 1 - \`${client.shards.get(1).latency}ms\``);
-		} else if (args[0].toLowerCase() === '2') {
-			message.channel.createMessage(`**Pong!** Shard 2 - \`${client.shards.get(2).latency}ms\``);
-		} else if (args[0].toLowerCase() === '3') {
-			message.channel.createMessage(`**Pong!** Shard 3 - \`${client.shards.get(3).latency}ms\``);
-		} else if (
-			args[0].toLowerCase() !== '3' ||
-			args[0].toLowerCase() !== '2' ||
-			args[0].toLowerCase() !== '1' ||
-			args[0].toLowerCase() !== '0'
-		) {
-			const msg = await message.channel.createMessage('🏓 Pong!');
-			msg.edit(`**That shard does not exist!** \`${Math.abs(message.createdAt - msg.createdAt)}ms\``);
+		const shards = client.shards;
+		switch (args[0]) {
+			case '0':
+				message.channel.createMessage(`Pong! - \`${shards.get(0).latency}ms\` for Shard 0`);
+				break;
+			case '1':
+				message.channel.createMessage(`Pong! - \`${shards.get(1).latency}ms\` for Shard 1`);
+				break;
+			default:
+				const msg = await message.channel.createMessage('🏓 Pong!');
+				msg.edit(`Pong! \`${Math.abs(message.createdAt - msg.createdAt)}ms\``);
 		}
 	}
 };
