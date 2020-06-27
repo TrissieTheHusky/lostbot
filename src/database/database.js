e3cdc19266a4d8a2559a6c4eeec5540acfd02f42
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-27 20:26:07 +0100

    sync dashboard and bot

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-module.exports = mongoose.connect(`mongodb://localhost/lost`, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true
+module.exports = mongoose.connect('mongodb://localhost:27017/lost', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
